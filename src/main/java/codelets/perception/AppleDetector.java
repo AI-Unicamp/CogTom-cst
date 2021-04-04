cff1b575b08381adcf00ec4eccb5136e6269e40e
Fabio Grassiotto <fabiogr@gmail.com>
2021-04-04 12:35:12 -0300

    Remove header for code start

--- src/main/java/codelets/perception/AppleDetector.java
+++ src/main/java/codelets/perception/AppleDetector.java
@@ -1,22 +1,3 @@
-/*****************************************************************************
- * Copyright 2007-2015 DCA-FEEC-UNICAMP
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- * Contributors:
- *    Klaus Raizer, Andre Paraense, Ricardo Ribeiro Gudwin
- *****************************************************************************/
-
 package codelets.perception;
 
 import br.unicamp.cst.core.entities.Codelet;
@@ -32,7 +13,7 @@ import ws3dproxy.model.Thing;
  * 	This class detects a number of things related to apples, such as if there are any within reach,
  * any on sight, if they are rotten, and so on.
  * 
- * @author klaus
+ * @author fabiogr
  *
  */
 public class AppleDetector extends Codelet {
